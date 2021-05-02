e8cd2250ca2d8e31ac14c13c2438c25b55483477
Thiago Mota <thiagomota99@outlook.com>
2021-05-01 21:01:23 -0300

    ListaNegociacoes
    Foi criado uma classe de modelo "ListaNegociacoes". Onde a mesma tem como métodos adicionar uma nova negociação e retorna a mesma.
    
    Na classe de "NegociacaoController" foi criado um novo atributo no construtor do tipo ListaNegociacao para adicionar as negociacoes recebidas do formulário.
    
    Adicionado também os métodos auxiliares de NegociacaoController
    
    - criaNegociacao() = Responsável por criar um objeto do tipo Negociacao
    
    - limparCamposDoForm() = Responsável por limpar os campos preenchidos na view e apontar o foco para o campo de data.

--- javascript-avancado-i-aula1/projeto/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/projeto/client/js/app/controllers/NegociacaoController.js
@@ -6,18 +6,28 @@ class NegociacaoController{
 		this._inputData = camposFormNegociacao.data;
 		this._inputQuantidade = camposFormNegociacao.quantidade;
 		this._inputValor = camposFormNegociacao.valor;
+		this._listaNegociacoes = new ListaNegociacoes();
 	}
 
 	incluir(event){
 		event.preventDefault();
 		
-		let negociacao = new Negociacao(
-			DateHelper.textoParaData(this._inputData.value),
-			this._inputQuantidade,
-			this._inputValor
-		);
+		this._listaNegociacoes.adiciona(_criaNegociacao());
+		_limparCamposDoForm();
 
 		console.log(negociacao);		
 		console.log(DateHelper.dataParaTexto(negociacao.data));
+	}
+
+	_criaNegociacao(){
+		return new Negociacao(
+			DateHelper.textoParaData(this._inputData.value),
+			this._inputQuantidade,
+			this._inputValor
+	}
+
+	_limparCamposDoForm(){
+		camposFormNegociacao.reset();
+		this._inputData.focus();		
 	}
 }
--- javascript-avancado-i-aula1/projeto/client/js/app/models/ListaNegociacoes.js
+++ javascript-avancado-i-aula1/projeto/client/js/app/models/ListaNegociacoes.js
@@ -3,4 +3,13 @@ class ListaNegociacoes {
 	constructor(){
 		this._negociacoes = [];
 	}
+
+	adiciona(negociacao){
+		this._negociacoes.push(negociacao);
+	}
+
+	get negociacoes(){
+
+		return [].concat(this._negociacoes);
+	}
 }
