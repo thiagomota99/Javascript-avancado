db1ce019161e32b728ec93aaed258c54bb2f623d
Thiago Mota <thiagomota99@outlook.com>
2021-05-04 10:10:40 -0300

    Adicionando mensagem ao controller
    Foram criados novos objetos do tipo Mensagem e MensagemView. Os mesmo servem para que possam ser criadas e atualizada view quando uma nova Negociação é criada.

--- javascript-avancado-i-aula1/projeto/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/projeto/client/js/app/controllers/NegociacaoController.js
@@ -7,9 +7,13 @@ class NegociacaoController{
 		this._inputQuantidade = camposDoFormulario.quantidade;
 		this._inputValor = camposDoFormulario.valor;
 		this._listaNegociacoes = new ListaNegociacoes();
-		this._negociacoesView = new NegociacoesView($("#tabelaDeNegociacoes")); //Passando como parâmetro para o construtor da classe o elemento alvo onde será exibido a view
 
+		this._negociacoesView = new NegociacoesView($("#tabelaDeNegociacoes")); //Passando como parâmetro para o construtor da classe o elemento alvo onde será exibido a view
 		this._negociacoesView.update(this._listaNegociacoes); //Chama o método update da NegociacoesView
+
+		this._mensagem = new Mensagem();
+		this._mensagemView = new MensagemView($("#mensagemView")); //Criando um objeto de Mensagem
+		this._mensagemView.update(this._mensagem);
 	}
 
 	incluir(event){
@@ -17,6 +21,8 @@ class NegociacaoController{
 		event.preventDefault();
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
 		this._negociacoesView.update(this._listaNegociacoes); //Chama o método update da NegociacoesView para refletir as mudanças no modelo
+		this._mensagem.texto = "Negociação adicionada com sucesso!";
+		this._mensagemView.update(this._mensagem);
 		this._limparCamposDoForm();			
 	}
 
